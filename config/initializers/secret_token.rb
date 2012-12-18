3bbfeab0aa41c2b4c559696f70e4de409d9c23ae
Derek Chen <derekchen14@gmail.com>
2012-12-18 11:19:13 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notable::Application.config.secret_token = '548161b8b3c6c9a90be68d3b759e6bd41233d322ff9f4efd51414cdd03a19de2a5d5ed7f8174c0c4c33068f4e9608752388eae343933876e9a9f739f1dcdcb5c'
