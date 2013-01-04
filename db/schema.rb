0df9ce79b130d7ea4854a692cee70d17e0918cdb
Derek Chen <derekchen14@gmail.com>
2013-01-04 15:41:20 -0800

    Include secure passwords into User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130104222653) do
+ActiveRecord::Schema.define(:version => 20130104232040) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "password"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
