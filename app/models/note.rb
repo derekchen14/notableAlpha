808d069aa404fbf4980cfb6f89e912d91f688731
Derek Chen <derekchen14@gmail.com>
2013-01-05 13:06:39 -0800

    Transfer in all previous specs

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,10 @@
 class Note < ActiveRecord::Base
-  attr_accessible :content, :format, :item_id, :user_id
+  attr_accessible :content, :format
+  belongs_to :user
+  has_many :items
+
+  validates_presence_of :user_id
+  validates_presence_of :content
+  validates_presence_of :format
+  
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Note do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  before { @note = user.notes.build(content: "Lorem ipsum") }  
+  subject { @note }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should == user }
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @note.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "accessible attributes" do
+    it "should not allow access to user_id" do
+      expect do
+        note.new(user_id: user.id)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
+  end
+
+  describe "when user_id is not present" do
+    before { @note.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with blank content" do
+    before { @note.content = " " }
+    it { should_not be_valid }
+  end
+  
 end
