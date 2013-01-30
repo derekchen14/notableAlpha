bf4a81f1df095aa8abf51dd46b4b752663c18df8
Derek Chen <derekchen14@gmail.com>
2013-01-29 22:28:41 -0800

    Fix Phone Number DataType Issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124234016) do
+ActiveRecord::Schema.define(:version => 20130130061826) do
 
   create_table "items", :force => true do |t|
     t.integer  "variable"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(:version => 20130124234016) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           :default => false
-    t.integer  "phone_number"
+    t.string   "phone_number"
     t.integer  "sendhub_id"
   end
 
