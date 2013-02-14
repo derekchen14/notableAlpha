bc0d2749b6a121085ab09712d1f9dffb2038cd16
Derek Chen <derekchen14@gmail.com>
2013-02-14 10:05:57 -0800

    Checkpoint for Sending flash messages

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -8,11 +8,23 @@ class RemindersController < ApplicationController
 	def create
 		respond_to do |format|
 			format.html { redirect_to root_url }
-			format.json do
-				Texter.send_text(current_user.sendhub_id, params[:reminder][:content])
-				render :json => {}, :status => :ok
-			end
+			format.json { send_reminder }
+			format.js
 		end	
 	end
+
+
+	private
+
+		def send_reminder
+			length = params[:reminder][:length]
+			if Texter.send_text(current_user.sendhub_id, params[:reminder][:content])
+				flash.now[:success] = "A reminder will be sent soon."
+				p params
+			else
+				flash[:error] = "Failed to set a reminder."
+			end
+			render json: {}, status: :ok			
+		end
 
 end
