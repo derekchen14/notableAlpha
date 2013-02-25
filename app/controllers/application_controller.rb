446ea61c50ffb859ae2d8fbd79c356318bdfd995
Nick Borromeo <ngborromeo@gmail.com>
2013-02-24 22:50:46 -0800

    Implement in-place edit for user's notes. Workaround for flash message issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 	  	return unless request.xhr?
 	  	# This will discontinue execution if Rails detects that the 
 	  	# request is not from an AJAX request
+      p "flash messages is #{flash_message}"
 	  	response.headers['x-flash'] = flash_message
       response.headers['x-flash-type'] = flash_type.to_s
 		  
@@ -16,8 +17,10 @@ class ApplicationController < ActionController::Base
 
 		def flash_message
 			[:error, :warning, :notice, :success].each do |type|
+        p "When flash[type] == #{type} flash[type].blank == #{flash[type].blank?}"
 				return flash[type] unless flash[type].blank?
 			end
+      "" #workaround to not show flash message when type is blank
 		end
 
 		def flash_type
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,6 +4,8 @@ class NotesController < ApplicationController
 	before_filter :signed_in_user
 	before_filter :correct_user,   only: :destroy
 
+  respond_to :html, :json
+  
 	def create
     @note = current_user.notes.build(params[:note])
     if params[:note][:content].blank?
@@ -16,16 +18,13 @@ class NotesController < ApplicationController
 	end
 
   def edit
-    @note = current_user.notes.find_by_id(params[:id])
+    @note = current_user.notes.find(params[:id])
   end
 
   def update
-    @note = current_user.notes.find_by_id(params[:id])
-    if @note.update_attributes(params[:note])
-      redirect_to root_url, notice: "Successfully updated note."
-    else
-      render :edit
-    end
+    @note = current_user.notes.find(params[:id])
+    @note.update_attributes(params[:note])
+    respond_with_bip @note
   end
 	
   def destroy
