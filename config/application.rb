74fcff2a38bdfbb774977e1f3827b523cf796a33
Nick Borromeo <ngborromeo@gmail.com>
2013-02-28 12:30:37 -0800

    Issue0114: Update Application files within Config for Devise

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,9 @@ module Notable
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku Deployment
+    config.assets.initialize_on_precompile = false
+
   end
 end
