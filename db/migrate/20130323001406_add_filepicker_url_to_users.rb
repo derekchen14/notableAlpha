d13002fa75c6fca3a82888f78a09da879ca9524f
Derek Chen <derekchen14@gmail.com>
2013-03-23 18:20:28 -0700

    Remove unecessary migration file

--- db/migrate/20130323001406_add_filepicker_url_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddFilepickerUrlToUsers < ActiveRecord::Migration
-  def up
-      add_column :users, :filepicker_url, :string
-  end
-
-  def down
-      remove_column :users, :filepicker_url
-  end    
-end
