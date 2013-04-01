02e3f5ed5d0340c18f3b910a871c86247b0674b1
Nick Borromeo <ngborromeo@gmail.com>
2013-03-31 21:32:16 -0700

    Add user id to tags model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130324003832) do
+ActiveRecord::Schema.define(:version => 20130331031226) do
 
   create_table "items", :force => true do |t|
     t.integer  "variable"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(:version => 20130324003832) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
@@ -63,7 +64,6 @@ ActiveRecord::Schema.define(:version => 20130324003832) do
     t.string   "email"
     t.boolean  "admin",                  :default => false
     t.integer  "sendhub_id"
-    t.string   "phone_number"
     t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -74,11 +74,11 @@ ActiveRecord::Schema.define(:version => 20130324003832) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone_number"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["phone_number"], :name => "phone_number_index", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
