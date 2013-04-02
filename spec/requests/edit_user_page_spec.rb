82871781f890f34d01b542827deadc395a038d61
Nick Borromeo <ngborromeo@gmail.com>
2013-04-02 13:10:23 -0700

    Refactor complex code and add more tests for editing a user

--- spec/requests/edit_user_page_spec.rb
+++ spec/requests/edit_user_page_spec.rb
@@ -16,7 +16,7 @@ describe "User profie update" do
     end
 
     it "email is blank" do 
-      fill_in "Email", with: nil
+      fill_in "Email", with: ""
       click_button "Save changes"
       expect(page).to have_content("Email can't be blank")
     end
@@ -33,14 +33,21 @@ describe "User profie update" do
       click_button "Save changes"
       expect(page).to have_content("Password doesn't match confirmation")
     end
-
-    # context "update succeeds when" do
-    #   it "phone number is valid" do
-    #     fill_in "Phone Number", with: 6266164630
-    #     click_button "Save changes"
-    #     save_and_open_page
-    #     expect(page).to have_content("Profile successfully added.")
-    #   end
-    # end
   end
+    
+    it "Add, Edit, Delete phone number" do
+      fill_in "Phone Number", with: 6503472313 
+      click_button "Save changes"
+      expect(page).to have_content("Phone number successfully added")
+      visit edit_user_registration_path
+      fill_in "Phone Number", with: 6503472311 
+      click_button "Save changes"
+      expect(page).to have_content("Phone number successfully updated")
+      visit edit_user_registration_path
+      fill_in "Phone Number", with: nil
+      click_button "Save changes"
+      expect(page).to have_content("Phone number successfully removed")
+    end
+
 end
+
