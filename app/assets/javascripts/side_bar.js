1be3f61d369ad4ebb3c910d51ed15c273bab4daf
Nick Borromeo <ngborromeo@gmail.com>
2013-04-07 21:26:23 -0700

    Add sub sections to side bar

---
+++ app/assets/javascripts/side_bar.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#side-notebooks").click(function(){
+   // $("#side-user-tags").toggle();
+  });
+  $("#side-recent-notes").click(function(){
+   // $("#side-user-tags").toggle();
+  });
+  $("#side-notes").click(function(){
+   // $("#side-user-tags").toggle();
+  });
+  $("#side-tags").click(function(){
+   $("#side-user-tags").toggle();
+  });
+});
