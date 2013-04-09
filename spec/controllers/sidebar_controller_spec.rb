f8db862d428540d2a06b6cab192227f6ddc754eb
Nick Borromeo <ngborromeo@gmail.com>
2013-04-09 11:43:00 -0700

    Add test for selecting a note from the sidebar

--- spec/controllers/sidebar_controller_spec.rb
+++ spec/controllers/sidebar_controller_spec.rb
@@ -32,4 +32,19 @@ describe SidebarController do
       response.should render_template(:short_notes)
     end
   end
+
+  describe "GET select_note" do
+    before(:each) do
+      @note = FactoryGirl.create(:note, user_id: user.id)
+    end
+    it "has a 200 status code" do
+      get :select_note, id: @note, format: :js
+      response.code.should eq("200")
+    end
+
+    it "render correct template" do
+      get :select_note, id: @note, format: :js
+      response.should render_template(:select_note)
+    end
+  end
 end
