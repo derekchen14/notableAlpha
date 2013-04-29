402243c86e48bb94d5cb296442e7fae6e564dc22
Nick Borromeo <ngborromeo@gmail.com>
2013-04-29 09:24:06 -0700

    Add keyboard shortcuts for bold, italic, underline, undo and redo.

--- app/assets/javascripts/note.js
+++ app/assets/javascripts/note.js
@@ -33,7 +33,15 @@ $(document).ready(function() {
   /*****************
    Mindmup Editor 
    *****************/
-  $("[id^=item_]").wysiwyg();
+  $("[id^=item_]").wysiwyg({
+    hotKeys: {
+      'ctrl+b meta+b': 'bold',
+      'ctrl+i meta+i': 'italic',
+      'ctrl+u meta+u': 'underline',
+      'ctrl+z meta+z': 'undo',
+      'ctrl+y meta+y meta+shift+z': 'redo'
+    }
+  });
 
   /*****************
     Auto Save
